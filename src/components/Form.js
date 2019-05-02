3abcd7a1dc181dbc3bc3a221a74284647d083ddd
camimaya21 <camilamaya21@gmail.com>
2019-05-02 23:57:39 +0200

    form done, pending send form to back

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,9 @@
 import React, { Component, createRef } from "react";
-import { Link } from "react-router-dom";
+import { connect } from 'react-redux'
+import { USER_UPDATED } from '../actions'
+
 import FormField from "./FormField";
+import { formToJson } from "../utils/form"
 
 import "./form.css";
 class Form extends Component {
@@ -11,11 +14,15 @@ class Form extends Component {
   }
 
   confirm(e) {
+ //   const { updateUser } = this.props
     e.preventDefault();
+    const data = formToJson(e.target)
+console.log(data)
+  //  updateUser(data)
   }
 
   onChange = value => {
-    const data = this.form.current;
+    const data = formToJson(this.form.current)
     this.setState({ ...data });
   };
 
@@ -32,49 +39,73 @@ class Form extends Component {
           <h1 className="title">Entonces... Bolt</h1>
         </section>
         <form className="form-wrapper" ref={this.form} onSubmit={this.confirm}>
-          <div className="form-item form-radio">
+          <div className="form-item">
             <h4>¿Contamos contigo?</h4>
-            <label>
-              <input type="radio" className="nes-radio" name="answer" defaultChecked />
-              <span>Claro que sí <span role="img" aria-label="cat">😻</span></span>
+            <label className="nes-field">
+              <select className="nes-input"  name="confirm" onChange={this.onChange}>
+                <option className="form-item" defaultChecked value="yes">Claro que sí guapis 😻</option>
+                <option className="form-item" value="no">No 💔</option>
+              </select>
             </label>
-            <label>
-              <input type="radio" className="nes-radio" name="answer" />
-              <span>No <span role="img" aria-label="heart-breaker">💔</span></span>
-            </label>
+            
+          </div>
+          <div className="form-item">
+            <h4>Número de Personas</h4>
+            <select className="nes-input"  name="numberPeople" onChange={this.onChange}>
+              <option className="form-item" defaultChecked value={1}>1</option>
+              <option className="form-item" value={2}>2</option>
+              <option className="form-item" value={3}>3</option>
+            </select>
           </div>
           <div className="form-item">
             <FormField
-              name="numpersonas"
-              label="Nº de personas"
-              onChange={this.onChange}
-            />
-          </div>
-          <div className="form-item">
-            <FormField
-              name="Alergias"
+              type="text"
+              name="alergies"
               label="Alergias"
               onChange={this.onChange}
             />
+          </div>
+          <div className="form-item">
+            <h4>Dieta Especial</h4>
+            <select className="nes-input"  name="specialDiet" onChange={this.onChange}>
+              <option className="form-item" defaultChecked value={1}>Ninguna</option>
+              <option className="form-item" value={2}>Vegetariano</option>
+              <option className="form-item" value={3}>Vegano</option>
+            </select>
           </div>
           <div className="form-item">
             <label htmlFor="textarea_field">Comentarios</label>
             <textarea
               id="textarea_field"
               className="nes-textarea"
-              name="Comentarios"
+              name="coments"
               onChange={this.onChange}
             />
           </div>
-        </form>
         <div className="btn-wrapper">
-          <Link to="/form">
-            <button>> Confirmar Asistencia</button>
-          </Link>
+            <button ref={this.action}>> Confirmar Asistencia</button>
         </div>
+        </form>
       </div>
     );
   }
 }
 
-export default Form;
+const mapState = ({user}) => {
+  return {
+    user
+  }
+}
+
+const mapDispatch = dispatch => {
+  return {
+    updateUser : payload => {
+      dispatch({
+        type: USER_UPDATED,
+        payload
+      }) 
+    } 
+  }
+}
+
+export default connect(mapState, mapDispatch)(Form);
